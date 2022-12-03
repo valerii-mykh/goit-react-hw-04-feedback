a56800689e7e513c9030f9d5e41839515e39472a
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-12-03 22:14:53 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import 'modern-normalize/modern-normalize.css';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
 import './index.css';
-import App from 'components/App/App';
 
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-
